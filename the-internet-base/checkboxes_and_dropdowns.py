b9ffdc4c845a175165b7bbaa3638ec9a75a7b043
Eddie Saunders <saunders.eddie@outlook.com>
2021-11-18 09:17:48 -0500

    Added in checkbox and dropdown

---
+++ the-internet-base/checkboxes_and_dropdowns.py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+driver = webdriver.Chrome(executable_path="C:\\webDrivers\\chromedriver_95.exe")
+driver.maximize_window()
+
+# Scenario: Ensure the functionality to add/remove button is correct
+driver.get("https://the-internet.herokuapp.com/")
+driver.find_element_by_link_text('Checkboxes').click()
+
+checkboxes = driver.find_elements_by_xpath("//input[@type='checkbox']")
+for box in checkboxes:
+    box.click()
+time.sleep(5)
+
+
+driver.get("https://the-internet.herokuapp.com/")
+driver.find_element_by_link_text('Dropdown').click()
+dropdown = Select(driver.find_element_by_id('dropdown'))
+dropdown.select_by_value('1')
+time.sleep(5)
+dropdown.select_by_value('2')
+time.sleep(5)
+driver.close()
+
